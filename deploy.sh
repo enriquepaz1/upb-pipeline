1b823b2a03021a6d6c90136da8bb7443a7b9f4e2
EC2 Default User <ec2-user@ip-172-31-41-194.us-east-2.compute.internal>
2020-05-08 01:00:31 +0000

    fixed buildSpec.yaml,template.yaml,deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ fi
 i=0 p=0 b=0 d=0
 
 CF_FILE="/tmp/cf_file.txt"
-DEPLOYMENTS_BUCKET="upb-cloudformation"
+DEPLOYMENTS_BUCKET="maudeployments"
 
 case "$1" in
   -i|--install)
@@ -42,34 +42,27 @@ esac
 if [[ $i -eq 1 ]]; then
   mkdir -p build
   cp -r src/* build/
-  echo install
 fi
 
-#transformaciones del build
 if [[ $b -eq 1 ]]; then
-  aws cloudformation package \
+  
+aws cloudformation package \
   --template-file template.yaml \
   --s3-bucket $DEPLOYMENTS_BUCKET \
   --output-template-file $CF_FILE
-  
+
 fi
 
 if [[ $d -eq 1 ]]; then
-    aws cloudformation deploy \
+    
+aws cloudformation deploy \
   --no-fail-on-empty-changeset \
   --template-file $CF_FILE \
   --parameter-overrides Project=cf_lab2  \
-  --stack-name "upb-pipeline" \
+  --stack-name "codepipeline-stack" \
   --capabilities CAPABILITY_NAMED_IAM
-    echo deploy    
 fi
 
 if [[ $r -eq 1 ]]; then
-    aws cloudformation deploy \
-  --no-fail-on-empty-changeset \
-  --template-file $CF_FILE \
-  --parameter-overrides Project=cf_lab2  \
-  --stack-name "my-awesome-stack2" \
-  --capabilities CAPABILITY_NAMED_IAM
     echo remove
 fi
